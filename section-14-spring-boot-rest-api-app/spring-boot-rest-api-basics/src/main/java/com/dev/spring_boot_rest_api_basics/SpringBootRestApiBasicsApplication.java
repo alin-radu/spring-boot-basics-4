2c1c8bed3a92e0e0ed3d5b3e8d2bd982555d9fae
Alin RADU <alin.radu.cristian@gmail.com>
2025-01-21 12:56:28 +0200

    add spring-boot-rest-api-basics

---
+++ section-14-spring-boot-rest-api-app/spring-boot-rest-api-basics/src/main/java/com/dev/spring_boot_rest_api_basics/SpringBootRestApiBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.spring_boot_rest_api_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiBasicsApplication.class, args);
+	}
+
+}
---
+++ section-14-spring-boot-rest-api-app/spring-boot-rest-api-basics/src/test/java/com/dev/spring_boot_rest_api_basics/SpringBootRestApiBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.spring_boot_rest_api_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
