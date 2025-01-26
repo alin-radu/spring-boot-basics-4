352ae3fe3ac63ed3d9c07741c71236664a40c4c3
Alin RADU <alin.radu.cristian@gmail.com>
2025-01-26 09:15:49 +0200

    add spring-boot-full-stack-basics

---
+++ section-22-spring-boot-full-stack/spring-boot-full-stack-basics/src/test/java/com/dev/spring_boot_full_stack_basics/SpringBootFullStackBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.spring_boot_full_stack_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullStackBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
